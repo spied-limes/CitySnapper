2214959debb27ddb19a977d71c436779dbf4d3ef
Christian Mejia <chrismejia10@gmail.com>
2018-11-18 16:38:19 -0500

    start of combination

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -101,10 +101,10 @@ export default class HomeScreen extends React.Component {
             <Text> Nah you can't check in</Text>
           )}
           <Text style={styles.tabBarInfoText}>
-            CurrentLatitude:{this.state.latitude}{" "}
+            CurrentLatitude: {this.state.latitude}
           </Text>
           <Text style={styles.tabBarInfoText}>
-            CurrentLongitude:{this.state.longitude}{" "}
+            CurrentLongitude: {this.state.longitude}
           </Text>
 
           {/* <Button
@@ -124,41 +124,6 @@ export default class HomeScreen extends React.Component {
       </View>
     );
   }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use
-          useful development tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync(
-      "https://docs.expo.io/versions/latest/guides/development-mode"
-    );
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      "https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes"
-    );
-  };
 }
 
 const styles = StyleSheet.create({
