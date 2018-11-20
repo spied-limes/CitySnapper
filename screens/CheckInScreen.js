7710c67580bb484e00a76a814d99d1f822e53264
Christian Mejia <chrismejia10@gmail.com>
2018-11-20 12:03:25 -0500

    Commit to check camera looping issue

--- screens/CheckInScreen.js
+++ screens/CheckInScreen.js
@@ -1,28 +1,52 @@
 import React from "react";
-import { Text, View, ScrollView, StyleSheet, Button } from "react-native";
-import { ExpoLinksView } from "@expo/samples";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Button
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 export default class CheckinScreen extends React.Component {
   render() {
     const { params } = this.props.navigation.state;
 
     return (
-      <ScrollView style={styles.container}>
-        <Text>Hello Matess! </Text>
-        <Text>you have checked in to {params.name}</Text>
-        <Button
-          title="Go back"
-          onPress={() => this.props.navigation.navigate("Map")}
-        />
-      </ScrollView>
+      <SafeAreaView style={{ flex: 1 }}>
+        <ScrollView style={{ flex: 1 }}>
+          <View style={styles.navBox}>
+            <View />
+            <View>
+              <Text>Borough</Text>
+            </View>
+          </View>
+          <View style={styles.infoBox}>
+            <Text>Hello Matess! </Text>
+            <Text>you have checked in to {params.name}</Text>
+            <Button
+              title="Go back"
+              onPress={() => this.props.navigation.navigate("Map")}
+            />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  navBox: {
+    flex: 1
+  },
+
+  infoBox: {
+    flex: 2,
     paddingTop: 15,
-    backgroundColor: "#fff"
+    backgroundColor: "blue"
+  },
+  activityBox: {
+    flex: 1
   }
 });
