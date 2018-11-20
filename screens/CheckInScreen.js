0aadc29d580c00ca3fba0b559d9db72bb914035e
Christian Mejia <chrismejia10@gmail.com>
2018-11-20 12:51:34 -0500

    Baseline background image in place on checkIn page

--- screens/CheckInScreen.js
+++ screens/CheckInScreen.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {
+  ImageBackground,
   Text,
   View,
   SafeAreaView,
   ScrollView,
   StyleSheet,
-  Button
+  Button,
+  Platform
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
@@ -15,13 +17,26 @@ export default class CheckinScreen extends React.Component {
 
     return (
       <SafeAreaView style={{ flex: 1 }}>
-        <ScrollView style={{ flex: 1 }}>
-          <View style={styles.navBox}>
-            <View />
-            <View>
-              <Text>Borough</Text>
+        <ScrollView>
+          <ImageBackground
+            source={require("../assets/images/timesSquare.jpg")}
+            style={{ width: "100%", height: 450 }}
+          >
+            <View style={styles.navBox}>
+              <View>
+                <Ionicons
+                  name={
+                    Platform.OS === "ios" ? `ios-arrow-back` : "md-arrow-back"
+                  }
+                  size={35}
+                  color="white"
+                />
+              </View>
+              <View>
+                <Text style={styles.borough}>Borough</Text>
+              </View>
             </View>
-          </View>
+          </ImageBackground>
           <View style={styles.infoBox}>
             <Text>Hello Matess! </Text>
             <Text>you have checked in to {params.name}</Text>
@@ -37,14 +52,25 @@ export default class CheckinScreen extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  locationDetailsBox: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: "cover"
+  },
   navBox: {
+    marginTop: 40,
     flex: 1
   },
-
+  borough: {
+    color: "white",
+    fontSize: 20
+  },
   infoBox: {
     flex: 2,
     paddingTop: 15,
-    backgroundColor: "blue"
+    backgroundColor: "blue",
+    color: "white"
   },
   activityBox: {
     flex: 1
