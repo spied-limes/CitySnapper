6766c1f7651abeba572e42fdb4554ad6dfe949ce
Christian Mejia <chrismejia10@gmail.com>
2018-11-20 13:59:50 -0500

    pre-Rich Firebase commit

--- screens/CheckInScreen.js
+++ screens/CheckInScreen.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable quotes */
 import React from "react";
 import {
   ImageBackground,
@@ -7,7 +9,8 @@ import {
   ScrollView,
   StyleSheet,
   Button,
-  Platform
+  Platform,
+  TouchableOpacity
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
@@ -24,27 +27,29 @@ export default class CheckinScreen extends React.Component {
           >
             <View style={styles.navBox}>
               <View>
-                <Ionicons
-                  name={
-                    Platform.OS === "ios" ? `ios-arrow-back` : "md-arrow-back"
-                  }
-                  size={35}
-                  color="white"
-                />
+                <TouchableOpacity
+                  onPress={() => this.props.navigation.navigate("Map")}
+                >
+                  <Ionicons
+                    name={
+                      Platform.OS === "ios" ? `ios-arrow-back` : "md-arrow-back"
+                    }
+                    size={40}
+                    color="white"
+                  />
+                </TouchableOpacity>
               </View>
               <View>
-                <Text style={styles.borough}>Borough</Text>
+                <Text style={styles.borough}>Manhattan</Text>
               </View>
             </View>
+            <View style={styles.infoBox}>
+              <Text style={styles.infoBoxText}>Hello Matess! </Text>
+              <Text style={styles.infoBoxText}>
+                you have checked in to {params.name}
+              </Text>
+            </View>
           </ImageBackground>
-          <View style={styles.infoBox}>
-            <Text>Hello Matess! </Text>
-            <Text>you have checked in to {params.name}</Text>
-            <Button
-              title="Go back"
-              onPress={() => this.props.navigation.navigate("Map")}
-            />
-          </View>
         </ScrollView>
       </SafeAreaView>
     );
@@ -59,17 +64,24 @@ const styles = StyleSheet.create({
     resizeMode: "cover"
   },
   navBox: {
-    marginTop: 40,
-    flex: 1
+    marginTop: 28,
+    paddingHorizontal: 20,
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.4)"
   },
   borough: {
     color: "white",
     fontSize: 20
   },
   infoBox: {
-    flex: 2,
+    flex: 9,
     paddingTop: 15,
-    backgroundColor: "blue",
+    color: "white"
+  },
+  infoBoxText: {
     color: "white"
   },
   activityBox: {
