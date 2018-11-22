bf0a5cf81c62cdbf0b0d2bc85b1c2a1a43aadeb5
Christian Mejia <chrismejia10@gmail.com>
2018-11-22 18:11:30 -0500

    Remove tabs from HomeScreen - clean Log In/Sign Up pages

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,11 +2,13 @@ import React from "react";
 import { createSwitchNavigator } from "react-navigation";
 
 import MainTabNavigator from "./MainTabNavigator";
+import HomeScreen from "../screens/HomeScreen";
 import CheckinScreen from "../screens/CheckInScreen";
 import GetDirections from "../screens/GetDirections";
 export default createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+  Home: HomeScreen,
   Main: MainTabNavigator,
   Screen: CheckinScreen,
   Directions: GetDirections
