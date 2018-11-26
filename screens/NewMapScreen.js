8fae5540ebadda6f0f86437f86c5dd19b074ebc1
Christian Mejia <chrismejia10@gmail.com>
2018-11-26 00:38:44 -0500

    Add working LOCAL coordinate array switcher - Will need to rework with redux off firebase - Currently does NOT animate once new region is chosen - Need to remove Current Location from coordinates array - Does update location name in new bottom nav bar

--- screens/NewMapScreen.js
+++ screens/NewMapScreen.js
@@ -6,19 +6,20 @@ import {
   ScrollView,
   StyleSheet,
   Text,
-  TouchableOpacity,
+  TouchableHighlight,
   View,
   Button,
   TextInput
 } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import { createStackNavigator } from "react-navigation";
 import MapViewDirections from "react-native-maps-directions";
 import MapView, { Marker, AnimatedRegion, Animated } from "react-native-maps";
-import DropdownMenu from "react-native-dropdown-menu";
+// import DropdownMenu from "react-native-dropdown-menu";
 import { Constants, Location, Permissions } from "expo";
 import CheckinScreen from "./CheckInScreen";
 
-export default class NewMapScreen extends React.Component {
+export default class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null
   };
@@ -34,7 +35,8 @@ export default class NewMapScreen extends React.Component {
       text: "Current Location",
       // for current storage
       currentLat: null,
-      currentLong: null
+      currentLong: null,
+      currCoordIndex: 0
     };
   }
 
@@ -67,42 +69,84 @@ export default class NewMapScreen extends React.Component {
       currentLat: location.coords.latitude
     });
   };
+  setNextRegionCoord(number, maxIndex, coordsArray) {
+    let index = this.state.currCoordIndex;
+    if (index + number < 0) {
+      this.setState({
+        currCoordIndex: maxIndex
+      });
+    } else if (index + number > maxIndex) {
+      this.setState({
+        currCoordIndex: 0
+      });
+    } else {
+      index += number;
+      this.setState({
+        currCoordIndex: index
+      });
+    }
+    let nextIndex = this.state.currCoordIndex;
+    let nextLocation = coordsArray[nextIndex];
+    console.log("next index is:", nextIndex);
+    console.log("next location is:", nextLocation);
+
+    let newRegion = {
+      latitude: nextLocation.latitude,
+      longitude: nextLocation.longitude,
+      latitudeDelta: nextLocation.latitudeDelta,
+      longitudeDelta: nextLocation.longitudeDelta
+    };
+
+    this.map.animateToRegion(newRegion, 750);
+    this.setState({ text: nextLocation.locationName });
+  }
 
   render() {
     const { push, navigate } = this.props.navigation;
 
-    // DROPDOWN DATA
-    const data = [
-      [
-        "Current Location",
-        "Times Square",
-        "Fullstack Academy",
-        "World Trade Center",
-        "Museum of Sex"
-      ]
-    ]; //will have to be data
+    // OLD DROPDOWN DATA
+    // const data = [
+    //   [
+    //     "Current Location",
+    //     "Times Square",
+    //     "Fullstack Academy",
+    //     "World Trade Center",
+    //     "Museum of Sex"
+    //   ]
+    // ];
+    //will have to be data
     const coordinates = [
-      { latitude: this.state.currentLat, longitude: this.state.currentLong },
+      {
+        locationName: "Current Location",
+        latitude: this.state.currentLat,
+        longitude: this.state.currentLong,
+        latitudeDelta: 0.003,
+        longitudeDelta: 0.0015
+      },
       {
         //times square
+        locationName: "Times Square",
         latitude: 40.7589,
         longitude: -73.9851,
         latitudeDelta: 0.003,
         longitudeDelta: 0.0015
       },
       {
+        locationName: "Fullstack Academy",
         latitude: 40.7051,
         longitude: -74.0092,
         latitudeDelta: 0.003,
         longitudeDelta: 0.0015
       },
       {
+        locationName: "World Trade Center",
         latitude: 40.7118,
         longitude: -74.0131,
         latitudeDelta: 0.003,
         longitudeDelta: 0.0015
       },
       {
+        locationName: "Museum of Sex",
         latitude: 40.7441,
         longitude: -73.9874,
         latitudeDelta: 0.003,
@@ -110,88 +154,10 @@ export default class NewMapScreen extends React.Component {
       }
     ];
 
+    const coordsMaxIndex = coordinates.length - 1;
+
     return (
       <View style={styles.container}>
-        {/*
-  ______               ____
- /_  __/___  ____     / __ )____  _  __
-  / / / __ \/ __ \   / __  / __ \| |/_/
- / / / /_/ / /_/ /  / /_/ / /_/ />  <
-/_/  \____/ .___/  /_____/\____/_/|_|
-         /_/
-       */}
-        <View style={styles.mapCommentContainer}>
-          {/*
-    ____                       __
-   / __ \_________  ____  ____/ /___ _      ______
-  / / / / ___/ __ \/ __ \/ __  / __ \ | /| / / __ \
- / /_/ / /  / /_/ / /_/ / /_/ / /_/ / |/ |/ / / / /
-/_____/_/   \____/ .___/\__,_/\____/|__/|__/_/ /_/
-                /_/
-          */}
-          <View style={styles.topButtons}>
-            <DropdownMenu
-              style={{ flex: 1 }}
-              bgColor={"white"}
-              tintColor={"#000000"}
-              optionTextStyle={{ color: "red" }}
-              activityTintColor={"green"} // checkImage={} // arrowImg={}
-              titleStyle={{ color: "#333333" }}
-              // maxHeight={300}
-              handler={(selection, row) =>
-                this.setState({
-                  text: data[selection][row],
-                  latitude: coordinates[row].latitude,
-                  longitude: coordinates[row].longitude
-                })
-              }
-              data={data}
-            >
-              {/*
-    ____        __  __                     ____
-   / __ )____  / /_/ /_____  ____ ___     / __ )____  _  __
-  / __  / __ \/ __/ __/ __ \/ __ `__ \   / __  / __ \| |/_/
- / /_/ / /_/ / /_/ /_/ /_/ / / / / / /  / /_/ / /_/ />  <
-/_____/\____/\__/\__/\____/_/ /_/ /_/  /_____/\____/_/|_|
-*/}
-              <View style={{ flex: 1 }}>
-                <Text>Location: {this.state.text}</Text>
-                <Text>Lat: {this.state.latitude}</Text>
-                <Text>Lng: {this.state.longitude}</Text>
-              </View>
-            </DropdownMenu>
-            {/*
-   ________              __         ____         ____        __  __
-  / ____/ /_  ___  _____/ /__      /  _/___     / __ )__  __/ /_/ /_____  ____
- / /   / __ \/ _ \/ ___/ //_/_____ / // __ \   / __  / / / / __/ __/ __ \/ __ \
-/ /___/ / / /  __/ /__/ ,< /_____// // / / /  / /_/ / /_/ / /_/ /_/ /_/ / / / /
-\____/_/ /_/\___/\___/_/|_|     /___/_/ /_/  /_____/\__,_/\__/\__/\____/_/ /_/
-        */}
-            {this.state.latitude === this.state.currentLat &&
-            this.state.longitude === this.state.currentLong ? (
-              <Button
-                style={{ flex: 1 }}
-                onPress={() => navigate("Screen", { name: this.state.text })}
-                title="Check In"
-                color="#841584"
-              />
-            ) : (
-              <Button
-                style={{ flex: 1, alignItems: "center" }}
-                onPress={() =>
-                  navigate("Directions", {
-                    destLat: this.state.latitude,
-                    destLong: this.state.longitude,
-                    currentLong: this.state.currentLong,
-                    currentLat: this.state.currentLat
-                  })
-                }
-                title="Get Directions"
-                color="#841584"
-              />
-            )}
-          </View>
-        </View>
         {/*
     __  ___               _    ___
    /  |/  /___ _____     | |  / (_)__ _      __
@@ -202,6 +168,9 @@ export default class NewMapScreen extends React.Component {
         */}
         <View style={styles.mapFlexContainer}>
           <MapView
+            ref={ref => {
+              this.map = ref;
+            }}
             region={{
               // This is hardcoded in the snapping dropdown version
               latitude: this.state.latitude,
@@ -212,7 +181,7 @@ export default class NewMapScreen extends React.Component {
             showsUserLocation={true}
             style={styles.map}
           >
-            <Marker
+            {/* <Marker
               coordinate={{ latitude: 40.6, longitude: -74 }}
               title={"first marker"}
               description={"jkh"}
@@ -221,8 +190,53 @@ export default class NewMapScreen extends React.Component {
                   ? console.log("success")
                   : console.log("failure")
               }
-            />
+            /> */}
           </MapView>
+        </View>
+        <View style={styles.placeSelectBox}>
+          <View style={styles.prevPlaceSelect}>
+            <TouchableHighlight
+              onPress={() => {
+                this.setNextRegionCoord(-1, coordsMaxIndex, coordinates);
+              }}
+            >
+              <Ionicons
+                name={
+                  Platform.OS === "ios" ? `ios-arrow-back` : "md-arrow-back"
+                }
+                size={40}
+                color="white"
+              />
+            </TouchableHighlight>
+          </View>
+          <View style={styles.placeName}>
+            <Text
+              style={{
+                fontSize: 20,
+                color: "white",
+                fontFamily: "Abril-FatFace"
+              }}
+            >
+              {this.state.text}
+            </Text>
+          </View>
+          <View style={styles.nextPlaceSelect}>
+            <TouchableHighlight
+              onPress={() => {
+                this.setNextRegionCoord(1, coordsMaxIndex, coordinates);
+              }}
+            >
+              <Ionicons
+                name={
+                  Platform.OS === "ios"
+                    ? `ios-arrow-forward`
+                    : "md-arrow-forward"
+                }
+                size={40}
+                color="white"
+              />
+            </TouchableHighlight>
+          </View>
         </View>
       </View>
     );
@@ -239,9 +253,6 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-around",
     paddingHorizontal: 25
-  },
-  mapFlexContainer: {
-    flex: 4
   },
   map: {
     left: 0,
@@ -261,5 +272,29 @@ const styles = StyleSheet.create({
     textAlign: "center",
     justifyContent: "center",
     color: "white"
+  },
+  mapFlexContainer: {
+    flex: 8
+  },
+  placeSelectBox: {
+    flex: 2,
+    flexDirection: "row",
+    backgroundColor: "black"
+  },
+  prevPlaceSelect: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  placeName: {
+    flex: 3,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "rgba(255,255,255,0.4)"
+  },
+  nextPlaceSelect: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
   }
 });
