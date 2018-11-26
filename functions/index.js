da63115e0071eed1cca7a159fccffa34514d28bd
showduhtung <shaun.tung@gmail.com>
2018-11-26 11:57:00 -0500

    the Hello World function works

---
+++ functions/index.js
@@ -0,0 +1,9 @@
+const functions = require('firebase-functions');
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  console.log('helloo');
+  response.send('Hello from Shaun"s Firebase!');
+});
