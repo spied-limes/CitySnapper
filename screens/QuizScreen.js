95162926ec3d735cad58bc224bb010422ba0a933
Christian Mejia <chrismejia10@gmail.com>
2018-11-28 00:17:37 -0500

    Add quiz styling, will manual merge because of how old the base is

--- screens/QuizScreen.js
+++ screens/QuizScreen.js
@@ -1,13 +1,17 @@
 import {
-  StyleSheet,
-  Text,
-  View,
   Button,
   Dimensions,
-  ScrollView
+  ImageBackground,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
 } from "react-native";
 import React from "react";
 import { List, ListItem } from "native-base";
+import { Ionicons } from "@expo/vector-icons";
+import { navigate } from "react-navigation";
 
 const { width, height } = Dimensions.get("window");
 let arrnew = [];
@@ -36,7 +40,7 @@ const jsonData = {
       question:
         "How many pedestrians pass through Times Square on a typical day?",
       answer:
-        "On an average day around 340,000 pedestrains pass through Time Sqaure, which makes it one of the worlds most visited tourist attractions"
+        "On an average day, around 340,000 pedestrains pass through Time Sqaure, which makes it one of the worlds most visited tourist attractions"
     },
     question3: {
       correctoption: "true",
@@ -58,7 +62,7 @@ const jsonData = {
       question:
         "In what year did the New York Times bring this area to life, by opening its offices atop a subway station?",
       answer:
-        "Times Sqaure was renamed from Longacre Sqaure in 1904, when the New York Times opened up an office in the area"
+        "Times Square was renamed from Longacre Square in 1904, when the New York Times opened up an office in the area."
     },
     question5: {
       correctoption: "141 feet",
@@ -73,6 +77,7 @@ const jsonData = {
     }
   }
 };
+
 export default class Quiz extends React.Component {
   constructor(props) {
     super(props);
@@ -91,7 +96,7 @@ export default class Quiz extends React.Component {
       options: arrnew[this.qno].options,
       correctoption: arrnew[this.qno].correctoption,
       quizComplete: false,
-      viewAnswers: false
+      viewAnswers: true
     };
   }
   prev() {
@@ -146,79 +151,174 @@ export default class Quiz extends React.Component {
     let _this = this;
     const currentOptions = this.state.options;
     const correctoption = this.state.correctoption;
+
+    /*
+    ___                                  ____        __  __
+   /   |  ____  ______      _____  _____/ __ )__  __/ /_/ /_____  ____  _____
+  / /| | / __ \/ ___/ | /| / / _ \/ ___/ __  / / / / __/ __/ __ \/ __ \/ ___/
+ / ___ |/ / / (__  )| |/ |/ /  __/ /  / /_/ / /_/ / /_/ /_/ /_/ / / / (__  )
+/_/  |_/_/ /_/____/ |__/|__/\___/_/  /_____/\__,_/\__/\__/\____/_/ /_/____/
+*/
     const options = Object.keys(currentOptions).map(function(k) {
       return (
-        <View key={k} style={{ margin: 10 }}>
-          <Button
-            title={currentOptions[k]}
-            onPress={() => _this.checkAns(currentOptions[k], correctoption)}
-          />
-        </View>
+        <TouchableOpacity
+          key={k}
+          style={styles.answerButton}
+          onPress={() => _this.checkAns(currentOptions[k], correctoption)}
+        >
+          <Text style={styles.answerText}>{currentOptions[k]}</Text>
+        </TouchableOpacity>
       );
     });
+    /*
+    ____                  ____       _____
+   / __ \___  _______  __/ / /______/ ___/_____________  ___  ____
+  / /_/ / _ \/ ___/ / / / / __/ ___/\__ \/ ___/ ___/ _ \/ _ \/ __ \
+ / _, _/  __(__  ) /_/ / / /_(__  )___/ / /__/ /  /  __/  __/ / / /
+/_/ |_|\___/____/\__,_/_/\__/____//____/\___/_/   \___/\___/_/ /_/
+*/
 
     if (this.state.quizComplete && !this.state.viewAnswers) {
       return (
-        <View style={{ paddingTop: 50 }}>
-          <Text>Congrats Your Score is: {this.score}/5</Text>
-          <Text>Retake Quiz?</Text>
-          <Button title="yes" onPress={() => this.resetGame()} />
-          <Button title="no" onPress={() => navigate("Home")} />
-          <Text>Answers</Text>
-          <Button
-            title="view answers"
-            onPress={() => {
-              this.setState({
-                viewAnswers: true
-              });
-            }}
-          />
-        </View>
-      );
-    }
-
-    if (this.state.viewAnswers) {
-      return (
-        <View style={{ paddingTop: 50 }}>
-          {arrnew.map(i => {
-            return <Text>{i["answer"]}</Text>;
-          })}
-
-          <Button
-            title="go back"
-            onPress={() => {
-              this.setState({
-                viewAnswers: false
-              });
-            }}
-          />
-        </View>
-      );
-    }
-
-    return (
-      <ScrollView style={{ paddingTop: 10 }}>
-        <View style={styles.container}>
-          <View
-            style={{
-              flex: 1,
-              flexDirection: "column",
-              justifyContent: "space-between",
-              alignItems: "center"
-            }}
-          >
-            <View style={{ margin: 10 }}>
-              <Text>{this.state.question}</Text>
+        <ImageBackground
+          source={require("../assets/images/timesSquare.jpg")}
+          style={styles.container}
+        >
+          <View style={styles.bgColorOverlay}>
+            <View style={styles.results}>
+              <Text style={styles.resultsScore}>
+                Congrats! You got {this.score}/5 correct.
+              </Text>
             </View>
-            <View>{options}</View>
-            <View style={{ flexDirection: "row" }}>
-              <View style={{ margin: 15 }} />
+            <View style={styles.retakeChoiceBox}>
+              <View style={styles.retakeTextBox}>
+                <Text style={styles.retakeText}>Retake Quiz?</Text>
+              </View>
 
-              <View style={{ flexDirection: "row" }} />
+              <View style={styles.retakeChoices}>
+                <TouchableOpacity
+                  style={styles.answerButton}
+                  onPress={() => this.resetGame()}
+                >
+                  <Text style={styles.answerText}>Yes</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.answerButton}
+                  onPress={() => navigate("CheckIn")}
+                >
+                  <Text style={styles.answerText}>No</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View style={styles.viewAnswersBox}>
+              <Text>Answers</Text>
+              <TouchableOpacity
+                style={styles.answerButton}
+                onPress={() => {
+                  this.setState({
+                    viewAnswers: true
+                  });
+                }}
+              >
+                <Text style={styles.answerText}>View All Answers</Text>
+              </TouchableOpacity>
             </View>
           </View>
+        </ImageBackground>
+      );
+    }
+    /*
+ _    ___              ___
+| |  / (_)__ _      __/   |  ____  ______      _____  __________
+| | / / / _ \ | /| / / /| | / __ \/ ___/ | /| / / _ \/ ___/ ___/
+| |/ / /  __/ |/ |/ / ___ |/ / / (__  )| |/ |/ /  __/ /  (__  )
+|___/_/\___/|__/|__/_/  |_/_/ /_/____/ |__/|__/\___/_/  /____/
+*/
+    if (this.state.viewAnswers) {
+      return (
+        <ImageBackground
+          source={require("../assets/images/timesSquare.jpg")}
+          style={styles.container}
+        >
+          <View style={styles.bgColorOverlay}>
+            <View style={styles.navBox}>
+              <View>
+                <TouchableOpacity
+                  onPress={() => this.props.navigation.navigate("Map")}
+                >
+                  <Ionicons
+                    name={
+                      Platform.OS === "ios" ? `ios-arrow-back` : "md-arrow-back"
+                    }
+                    size={40}
+                    color="white"
+                  />
+                </TouchableOpacity>
+              </View>
+              <View>
+                <Text style={styles.quizLocation}>Answers</Text>
+              </View>
+            </View>
+            <View style={styles.longAnswersBox}>
+              {arrnew.map(i => {
+                return (
+                  <View key={i["answer"]}>
+                    <Text style={styles.longAnswers}>{i["answer"]}</Text>
+                  </View>
+                );
+              })}
+            </View>
+            <View style={styles.returnToCheckInBox}>
+              <TouchableOpacity
+                style={styles.answerButton}
+                onPress={() => this.props.navigation.navigate("CheckIn")}
+              >
+                <Text style={styles.answerText}>Return to Check In</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </ImageBackground>
+      );
+    }
+
+    /*
+    ____                 __
+   / __ \___  ____  ____/ /__  _____
+  / /_/ / _ \/ __ \/ __  / _ \/ ___/
+ / _, _/  __/ / / / /_/ /  __/ /
+/_/ |_|\___/_/ /_/\__,_/\___/_/
+*/
+    return (
+      <ImageBackground
+        source={require("../assets/images/timesSquare.jpg")}
+        style={styles.container}
+      >
+        <View style={styles.bgColorOverlay}>
+          {/* ########## NAV BOX ########## */}
+          <View style={styles.navBox}>
+            <View>
+              <TouchableOpacity
+                onPress={() => this.props.navigation.navigate("Map")}
+              >
+                <Ionicons
+                  name={
+                    Platform.OS === "ios" ? `ios-arrow-back` : "md-arrow-back"
+                  }
+                  size={40}
+                  color="white"
+                />
+              </TouchableOpacity>
+            </View>
+            <View>
+              <Text style={styles.quizLocation}>Times Square</Text>
+            </View>
+          </View>
+          <View style={styles.questionBox}>
+            <Text style={styles.question}>{this.state.question}</Text>
+          </View>
+          <View style={styles.answerBox}>{options}</View>
         </View>
-      </ScrollView>
+      </ImageBackground>
     );
   }
 }
@@ -231,7 +331,123 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
+    alignItems: "center",
+    width: "100%",
+    height: undefined,
+    resizeMode: "cover"
+  },
+  bgColorOverlay: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "rgba(0,0,0,.35)"
+  },
+  navBox: {
+    marginTop: 35,
+    paddingHorizontal: 20,
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center"
+  },
+  quizLocation: {
+    color: "white",
+    fontFamily: "Abril-FatFace",
+    fontSize: 24,
+    textShadowColor: "rgba(0, 0, 0, 0.95)",
+    textShadowOffset: { width: -2, height: 2 },
+    textShadowRadius: 2
+  },
+  questionBox: {
+    flex: 3,
+    justifyContent: "center",
+    paddingHorizontal: 50
+  },
+  question: {
+    alignItems: "center",
+    color: "white",
+    fontSize: 28,
+    textShadowColor: "rgba(0, 0, 0, 0.95)",
+    textShadowOffset: { width: -5, height: 5 },
+    textShadowRadius: 5
+  },
+  answerBox: {
+    flex: 6,
+    alignItems: "stretch"
+  },
+  answerButton: {
+    flex: 1,
+    backgroundColor: "rgba(0,0,0,.5)",
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 10
+  },
+  answerText: {
+    color: "white",
+    fontSize: 28,
+    textShadowColor: "rgba(0, 0, 0, 0.95)",
+    textShadowOffset: { width: -2, height: 2 },
+    textShadowRadius: 2
+  },
+  results: {
+    flex: 4,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 35,
+    paddingHorizontal: 20
+  },
+  resultsScore: {
+    color: "white",
+    fontSize: 30,
+    textShadowColor: "rgba(0, 0, 0, 0.95)",
+    textShadowOffset: { width: -4, height: 4 },
+    textShadowRadius: 4
+  },
+  retakeChoiceBox: {
+    flex: 2,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  retakeTextBox: {
+    flex: 1
+  },
+  retakeText: {
+    color: "white",
+    fontSize: 24,
+    textShadowColor: "rgba(0, 0, 0, 0.95)",
+    textShadowOffset: { width: -5, height: 5 },
+    textShadowRadius: 5
+  },
+  retakeChoices: {
+    flexDirection: "row",
+    alignItems: "stretch",
+    flex: 1,
+    marginHorizontal: 25
+  },
+  viewAnswersBox: {
+    flex: 2
+  },
+  viewAnswersButtons: {
+    flex: 1,
+    flexDirection: "row"
+  },
+  longAnswersBox: {
+    flex: 8,
+    justifyContent: "space-evenly"
+  },
+  longAnswers: {
+    color: "white",
+    fontSize: 16,
+    marginBottom: 15,
+    marginHorizontal: 30,
+    textShadowColor: "rgba(0, 0, 0, 0.95)",
+    textShadowOffset: { width: -3, height: 3 },
+    textShadowRadius: 3
+  },
+  returnToCheckInBox: {
+    flex: 1
+  },
+  returnToCheckIn: {
+    alignItems: "stretch"
   },
   welcome: {
     fontSize: 20,
